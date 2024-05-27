f983b831750d09e4a8a4a1d6fd653b76cc671621
CotaGM <cotadiseno@gmail.com>
2024-05-27 12:37:16 +0200

    primero prueba de show index

--- Developers/app/controllers/ApplicationController.php
+++ Developers/app/controllers/ApplicationController.php
@@ -4,7 +4,34 @@
  * Base controller for the application.
  * Add general things in this controller.
  */
-class ApplicationController extends Controller 
-{
-	
+class ApplicationController extends Controller {
+
+    public function indexAction(){
+        $task = new ApplicationModel();
+		// buscar en el modelo los datos
+        $taskList = $task -> getTasks();
+        // devuelves a la vista
+        $this -> view -> taskList = $taskList;
+	}
+
+    public function createAction(){
+        $task = new ApplicationModel();
+        // buscar en el modelo los datos
+        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $name = $_POST["nombre"];
+        $description = $_POST["descripcion"];
+        $state = $_POST["estado"];
+        $dueDate= $_POST["fecha Tope"];
+
+        $newTask = [
+            'nombre' => $name,
+            'descripcion' => $description,
+            'estado' => $state,
+            'fecha Tope' => $dueDate
+        ];
+        $task->createTask($newTask);
+        }
+        // devuelves a la vista
+        $this->view->message = "hello this will the list of create creatfdfsdafadsfadsafae";
+    }
 }
---
+++ Developers/app/models/ApplicationModel.php
@@ -0,0 +1,25 @@
+<?php
+class ApplicationModel extends Model{
+    
+    protected array $tasks;
+    
+    public function __construct(){
+       $this -> tasks = []; 
+    }
+    
+    public function getTasks(){
+    //obtener archivo json
+    $jsonData = file_get_contents(__DIR__. "/dataBase.json");
+    //convertir data json a php
+    $this -> tasks = json_decode($jsonData, true);
+    return $this -> tasks;
+    }
+    
+    public function createTask(array $newTask){
+        $this->tasks[] = $newTask;
+        $jsonData = json_encode($this->tasks, JSON_PRETTY_PRINT);
+        file_put_contents($jsonData, $jsonData);
+    }
+
+}
+?>
